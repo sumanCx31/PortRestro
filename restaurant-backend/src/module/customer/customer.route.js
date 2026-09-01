55eda8a863e17cbcd6703c4c95d6242ae56ef9ee
sumancx31 <sumansah029@gmail.com>
2026-09-01 13:26:09 +0545

    chnage

--- restaurant-backend/src/module/customer/customer.route.js
+++ restaurant-backend/src/module/customer/customer.route.js
@@ -2,6 +2,6 @@ const CustomerRouter = require('express').Router();
 const Customercltr = require('./customer.controller');
 
 CustomerRouter.post("/", Customercltr.createCustomer);
-CustomerRouter.post("/:cafeUserName", Customercltr.getAllCustomers);
+CustomerRouter.get("/:cafeUserName", Customercltr.getAllCustomers);
 
 module.exports = CustomerRouter;
